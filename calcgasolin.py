5f71bd4acd59e7d95b79bc9d07fb7ff4dff6295e
shirin-ux <shirin.alizadeh19@gmail.com>
2020-07-14 20:05:16 +0430

    change code

--- calcgasolin.py
+++ calcgasolin.py
@@ -2,4 +2,4 @@ a = int(input("Enter back gasolin a:"))
 b = float(input("Enter Consumption gasolin b:"))
 
 distance =float((a/b)*100)
-print("the distance is",distance)
+print("the distance is", distance)
